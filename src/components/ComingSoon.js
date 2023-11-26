aedaf17862e1ec5b25c64c33d629678ea0660f03
Marton Munz <marton@bioinformaticscro.com>
2023-11-26 14:02:27 +0100

    Important changes

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -2,7 +2,7 @@
 import React from "react"
 
 const ComingSoon = () => (
-  <div className="flex flex-col items-center justify-center h-screen">
+  <div className="flex flex-col h-screen">
     <h1 className="text-4xl font-bold">Coming Soon</h1>
     <p className="text-lg mt-4">
       This site will be available on mobile devices shortly.
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,18 +11,27 @@ import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Header from "./header"
 import "./layout.css"
+import ComingSoon from "../components/ComingSoon"
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-800 text-white">
-      <Header siteTitle={`Márton Münz's website`} />
-      <div className="p-8 flex-grow mr-40 max-w-3xl">
-        <main>{children}</main>
+    <div>
+      <div className="hidden md:block">
+        <div className="flex flex-col min-h-screen bg-gray-800 text-white">
+          <Header siteTitle={`Márton Münz's website`} />
+          <div className="p-8 flex-grow mr-10 max-w-3xl">
+            <main>{children}</main>
+          </div>
+          <div>
+            <footer className="p-4 flex justify-end bg-gray-700">
+              © {new Date().getFullYear()}, Márton Münz
+            </footer>
+          </div>
+        </div>
       </div>
-      <div>
-        <footer className="p-4 flex justify-end bg-gray-700">
-          © {new Date().getFullYear()}, Márton Münz
-        </footer>
+
+      <div className="block md:hidden">
+        <ComingSoon />
       </div>
     </div>
   )
